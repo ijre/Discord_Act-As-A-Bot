f3c54d9cce2a0ce664f526e049c7d88130d38b7b
Paige <33820042+ijre@users.noreply.github.com>
2019-11-03 01:31:38 -0500

    added second proj + .sln edit for 2nd proj

---
+++ hatsune_miku-bot_display/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace hatsune_miku_bot_display
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.browse_button = new System.Windows.Forms.Button();
+            this.Output_Chat = new System.Windows.Forms.TextBox();
+            this.version = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // browse_button
+            // 
+            this.browse_button.Location = new System.Drawing.Point(366, 275);
+            this.browse_button.Name = "browse_button";
+            this.browse_button.Size = new System.Drawing.Size(189, 171);
+            this.browse_button.TabIndex = 0;
+            this.browse_button.Text = "search for hatsune_miku.exe";
+            this.browse_button.UseVisualStyleBackColor = true;
+            this.browse_button.Click += new System.EventHandler(this.Start);
+            // 
+            // Output_Chat
+            // 
+            this.Output_Chat.BackColor = System.Drawing.SystemColors.GrayText;
+            this.Output_Chat.ForeColor = System.Drawing.SystemColors.Window;
+            this.Output_Chat.Location = new System.Drawing.Point(12, 12);
+            this.Output_Chat.Multiline = true;
+            this.Output_Chat.Name = "Output_Chat";
+            this.Output_Chat.ReadOnly = true;
+            this.Output_Chat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Output_Chat.Size = new System.Drawing.Size(891, 257);
+            this.Output_Chat.TabIndex = 1;
+            this.Output_Chat.Visible = false;
+            // 
+            // version
+            // 
+            this.version.Location = new System.Drawing.Point(389, 512);
+            this.version.Name = "version";
+            this.version.Size = new System.Drawing.Size(130, 20);
+            this.version.TabIndex = 4;
+            this.version.Text = "Hatsune_Miku-Bot 0.0.1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(915, 544);
+            this.Controls.Add(this.version);
+            this.Controls.Add(this.Output_Chat);
+            this.Controls.Add(this.browse_button);
+            this.DoubleBuffered = true;
+            this.MinimumSize = new System.Drawing.Size(931, 583);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse_button;
+        private System.Windows.Forms.TextBox Output_Chat;
+        private System.Windows.Forms.TextBox version;
+    }
+}
+
---
+++ hatsune_miku-bot_display/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace hatsune_miku_bot_display
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                InitialDirectory = Application.StartupPath,
+                DefaultExt = "Hatsune_Miku.exe",
+                Filter = "Hatsune_Miku.exe | Hatsune_Miku.exe",
+                Title = "Select the Hatsune_Miku.exe file."
+            };
+            diag.ShowDialog();
+
+            if (diag.FileName.Length > 0)
+            {
+                try
+                {
+                    Process process = new Process
+                    {
+                        EnableRaisingEvents = true
+                    };
+                    process.StartInfo = new ProcessStartInfo
+                    {
+                        UseShellExecute = false,
+                        FileName = diag.FileName,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true
+                    };
+                    process.ErrorDataReceived += (harvestEvent, errput) => Output_Chat.Text += errput.Data + "\r\n";
+                    process.OutputDataReceived += (e_vent, output) => Output_Chat.Text += output.Data + "\r\n";
+
+                    process.Start();
+                    process.BeginErrorReadLine();
+                    process.BeginOutputReadLine();
+
+                    Output_Chat.Visible = true;
+                    browse_button.Visible = false;
+                }
+                catch
+                {
+                    var message = MessageBox.Show("Process failed to launch.", "Fatal error.", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+
+                    if (message == DialogResult.Retry)
+                    {
+                        Start(sender, e);
+                        return;
+                    }
+                    else if (message == DialogResult.Cancel)
+                        return;
+                    else throw;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Process.GetProcessesByName("Hatsune_Miku")[0].Kill();
+            }
+            catch
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
