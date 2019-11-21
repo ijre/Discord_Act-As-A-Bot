aeaf7d162e85c9a6396f3af2b58e3e902f535d0b
Paige <33820042+ijre@users.noreply.github.com>
2019-11-21 14:52:39 -0500

    update before release build fix

--- hatsune_miku-bot_display/Form1.Designer.cs
+++ hatsune_miku-bot_display/Form1.Designer.cs
@@ -29,41 +29,81 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.browse_button = new System.Windows.Forms.Button();
             this.Output_Chat = new System.Windows.Forms.TextBox();
             this.version = new System.Windows.Forms.TextBox();
+            this.start_button = new System.Windows.Forms.Button();
+            this.Input_Chat = new System.Windows.Forms.TextBox();
+            this.send_button = new System.Windows.Forms.Button();
+            this.change_channel = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // browse_button
-            // 
-            this.browse_button.Location = new System.Drawing.Point(366, 275);
-            this.browse_button.Name = "browse_button";
-            this.browse_button.Size = new System.Drawing.Size(189, 171);
-            this.browse_button.TabIndex = 0;
-            this.browse_button.Text = "search for hatsune_miku.exe";
-            this.browse_button.UseVisualStyleBackColor = true;
-            this.browse_button.Click += new System.EventHandler(this.Start);
             // 
             // Output_Chat
             // 
             this.Output_Chat.BackColor = System.Drawing.SystemColors.GrayText;
-            this.Output_Chat.ForeColor = System.Drawing.SystemColors.Window;
+            this.Output_Chat.ForeColor = System.Drawing.Color.White;
             this.Output_Chat.Location = new System.Drawing.Point(12, 12);
             this.Output_Chat.Multiline = true;
             this.Output_Chat.Name = "Output_Chat";
             this.Output_Chat.ReadOnly = true;
             this.Output_Chat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.Output_Chat.Size = new System.Drawing.Size(891, 257);
+            this.Output_Chat.Size = new System.Drawing.Size(380, 257);
             this.Output_Chat.TabIndex = 1;
             this.Output_Chat.Visible = false;
             // 
             // version
             // 
+            this.version.Enabled = false;
             this.version.Location = new System.Drawing.Point(389, 512);
             this.version.Name = "version";
+            this.version.ReadOnly = true;
             this.version.Size = new System.Drawing.Size(130, 20);
             this.version.TabIndex = 4;
-            this.version.Text = "Hatsune_Miku-Bot 0.0.1.0";
+            this.version.TabStop = false;
+            this.version.Text = "Hatsune_Miku-Bot 0.0.2.0";
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(389, 290);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(130, 120);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "Launch Hatsune Miku Bot";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.Start);
+            // 
+            // Input_Chat
+            // 
+            this.Input_Chat.BackColor = System.Drawing.SystemColors.GrayText;
+            this.Input_Chat.ForeColor = System.Drawing.Color.White;
+            this.Input_Chat.Location = new System.Drawing.Point(516, 12);
+            this.Input_Chat.Multiline = true;
+            this.Input_Chat.Name = "Input_Chat";
+            this.Input_Chat.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Input_Chat.Size = new System.Drawing.Size(387, 257);
+            this.Input_Chat.TabIndex = 5;
+            this.Input_Chat.Visible = false;
+            this.Input_Chat.WordWrap = false;
+            // 
+            // send_button
+            // 
+            this.send_button.Location = new System.Drawing.Point(658, 275);
+            this.send_button.Name = "send_button";
+            this.send_button.Size = new System.Drawing.Size(84, 85);
+            this.send_button.TabIndex = 6;
+            this.send_button.Text = "Send";
+            this.send_button.UseVisualStyleBackColor = true;
+            this.send_button.Visible = false;
+            // 
+            // change_channel
+            // 
+            this.change_channel.Location = new System.Drawing.Point(12, 290);
+            this.change_channel.Name = "change_channel";
+            this.change_channel.Size = new System.Drawing.Size(130, 120);
+            this.change_channel.TabIndex = 7;
+            this.change_channel.Text = "Change Channel";
+            this.change_channel.UseVisualStyleBackColor = true;
+            this.change_channel.Visible = false;
+            this.change_channel.Click += new System.EventHandler(this.change_channel_Click);
             // 
             // Form1
             // 
@@ -73,9 +113,12 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(915, 544);
+            this.Controls.Add(this.change_channel);
+            this.Controls.Add(this.send_button);
+            this.Controls.Add(this.Input_Chat);
             this.Controls.Add(this.version);
             this.Controls.Add(this.Output_Chat);
-            this.Controls.Add(this.browse_button);
+            this.Controls.Add(this.start_button);
             this.DoubleBuffered = true;
             this.MinimumSize = new System.Drawing.Size(931, 583);
             this.Name = "Form1";
@@ -88,10 +131,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button browse_button;
         private System.Windows.Forms.TextBox Output_Chat;
         private System.Windows.Forms.TextBox version;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.TextBox Input_Chat;
+        private System.Windows.Forms.Button send_button;
+        private System.Windows.Forms.Button change_channel;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -1,17 +1,5 @@
 #include "main.h"
 
-std::string idGet()
-{
-	std::fstream idFile;
-	idFile.open("./id.txt");
-	std::string id;
-	idFile >> id;
-	idFile.close();
-	return id;
-}
-
-ClientClass client(idGet(), 2);
-
 int askServers(std::vector<Server> servers)
 {
 	for (int i = 0; i < servers.size(); i++)
@@ -20,15 +8,19 @@ int askServers(std::vector<Server> servers)
 		else
 			std::cout << i + 1 << ". " << servers[i].name << "\r\n";
 
-	std::string cin;
 	std::getline(std::cin, cin);
 
-	numIn = 0;
 	if (cin != "")
 		numIn = std::stoi(cin);
 
-	if (numIn <= 0)
+	if (numIn <= 0 || numIn > servers.size())
 		return -1;
+
+	std::ofstream guildID;
+	guildID.open("./guild.txt");
+	guildID << servers[numIn - 1].ID.string();
+	guildID.close();
+	// fed up with the time this is taking, going to take the "easy" and lazy way out
 
 	return numIn - 1;
 }
@@ -38,19 +30,17 @@ int64_t askChannels(std::vector<Channel> channels)
 	for (int i = 0; i < channels.size(); i++)
 	{
 		if (i == 0)
-			std::cout << "Choose your channel: \r\n" << i + 1 << ". " << channels[i].name << "\r\n";
+			std::cout << "\r\nChoose your channel: \r\n" << i + 1 << ". " << channels[i].name << "\r\n";
 		else
 			std::cout << i + 1 << ". " << channels[i].name << "\r\n";
 	}
 
-	std::string cin;
 	std::getline(std::cin, cin);
 
-	numIn = 0;
 	if (cin != "")
 		numIn = std::stoi(cin);
 
-	if (numIn < 0)
+	if (numIn <= 0 || (channels[numIn].type > 1 && channels[numIn].type != 3) || numIn > channels.size())
 		return -1;
 
 	return channels[numIn - 1].ID.number();
@@ -58,15 +48,36 @@ int64_t askChannels(std::vector<Channel> channels)
 
 int main()
 {
-	numIn = askServers(client.servers);
+	auto path = std::filesystem::path("./channel.txt");
+	if (std::filesystem::exists(path))
+		std::filesystem::remove(path);
+
+#define path std::filesystem::path("./guild.txt")
+	// im writing this before i test, and if this #define is legal im going to DIE OF LAUGHTER
+		// because writing path2 just doesnt work for my puny brain so it autopilots into saying path
+			// plus im just curious to see if it works
+
+	// IT WORKS ADIOWHDAIUHWDIUAHBDJBFNA
+
+	if (std::filesystem::exists(path))
+		std::filesystem::remove(path);
+
+	numIn = askServers(servers);
 	while (numIn == -1)
-		numIn = askServers(client.servers);
+		numIn = askServers(servers);
 
-	//while (GetAsyncKeyState(VK_RETURN))
-	//	continue;
-	// old check from debugging days to prevent accidental rollover into the askChannels function
+	while (GetAsyncKeyState(VK_RETURN))
+		continue;
+	// to prevent accidental rollover into the askChannels function
 
-	channel = askChannels(client.getServerChannels(client.servers[numIn].ID));
+	channel = askChannels(client.getServerChannels(servers[numIn].ID));
+	while (channel == -1)
+		channel = askChannels(client.getServerChannels(servers[numIn].ID));
 
-	client.run();
+	std::ofstream channelID;
+	channelID.open("./channel.txt");
+	channelID << std::to_string(channel);
+	channelID.close();
+
+	// fed up with the time this is taking, going to take the "easy" and lazy way out
 }
