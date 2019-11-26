374f919f5c1b13d1a93a7a104bfc1e0f2225642f
Paige <33820042+ijre@users.noreply.github.com>
2019-11-26 12:41:25 -0500

    removed now unnecessary folder

--- SD/sleepy_discord/http.cpp
+++
@@ -1,6 +0,0 @@
-#include "http.h"
-
-const char * SleepyDiscord::GenericSession::getMethodName(const RequestMethod & method) {
-	static constexpr char const * methodNames[] = { "POST", "PATCH", "DELETE", "GET", "PUT" };
-	return methodNames[method];
-}
