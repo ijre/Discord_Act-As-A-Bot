61caef097f4dba1377da2c75b785ce043fb82704
Paige <33820042+ijre@users.noreply.github.com>
2019-12-23 04:37:06 -0500

    ignore

--- src/GUI/Program.cs
+++ src/GUI/Program.cs
@@ -16,7 +16,7 @@ namespace hatsune_miku_bot_display
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
