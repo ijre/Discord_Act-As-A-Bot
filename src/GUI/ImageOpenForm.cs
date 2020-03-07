610b44152c822d1e7630dbd7c39179a2cf63cf7b
Paige <33820042+ijre@users.noreply.github.com>
2020-03-06 22:00:22 -0500

    Added image viewer, still a work in progress but is functional

---
+++ src/GUI/ImageOpenForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace IOF
+{
+    public partial class ImageOpenForm : Form
+    {
+        public ImageOpenForm()
+        {
+            InitializeComponent();
+
+            OpenFileDialog diag = new OpenFileDialog()
+            {
+                InitialDirectory = Application.StartupPath + "\\deps\\messages\\images",
+                RestoreDirectory = false
+            };
+            diag.ShowDialog();
+
+            if (!String.IsNullOrEmpty(diag.FileName))
+            {
+                Uri newUri;
+                Uri.TryCreate(File.ReadAllText(diag.FileName), UriKind.Absolute, out newUri);
+                webBrowser1.Url = newUri;
+                webBrowser1.Visible = true;
+
+                webBrowser1.DocumentCompleted += (object sender, WebBrowserDocumentCompletedEventArgs args) =>
+                {
+                    Height = webBrowser1.Document.Images[0].ScrollRectangle.Height + 70;
+                    Width = webBrowser1.Document.Images[0].ScrollRectangle.Width + 70;
+
+                    if (int.Parse(Screen.PrimaryScreen.Bounds.Width.ToString()) <= int.Parse(Size.Width.ToString())
+                    || int.Parse(Screen.PrimaryScreen.Bounds.Height.ToString()) <= int.Parse(Size.Height.ToString()))
+                    {
+                        Height = Screen.PrimaryScreen.Bounds.Height / 2;
+                        Width = Screen.PrimaryScreen.Bounds.Width / 2;
+                    }
+
+                    SetDesktopLocation((Screen.PrimaryScreen.Bounds.Width / 2) - (Size.Width / 2), (Screen.PrimaryScreen.Bounds.Height / 2) - (Size.Height / 2));
+                };
+            }
+        }
+    }
+}
--- src/GUI/Properties/Resources.Designer.cs
+++ src/GUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace hatsune_miku.Properties {
+namespace IOF.Properties {
     using System;
     
     
