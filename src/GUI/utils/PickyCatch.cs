537cd1813f6799bf7bfa3b8d4411a39e4a2acf10
Paige <33820042+ijre@users.noreply.github.com>
2020-03-26 19:01:17 -0400

    Moved PickyCatch into utils folder and edited it slightly

---
+++ src/GUI/utils/PickyCatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace hatsune_miku.utils
+{
+    public class PickyCatch
+    {
+        /// <summary>
+        /// Ignores a specific exception, to be used if you have a Catch(Exception) but don't want a specific exception.
+        /// Returns true if the exception was ignored.
+        /// </summary>
+        /// <param name="exceptionToIgnore">
+        /// Exception you wish to ignore.
+        /// </param>
+        /// <param name="exceptionCaught">
+        /// Exception caught.
+        /// </param>
+        /// <param name="messageBox">
+        /// If true, will automatically call MessageBox.Show() if the exception was not ignored.
+        /// Defaults to false.
+        /// </param>
+        /// <param name="title">
+        /// MessageBox title.
+        /// Defaults to "Error".
+        /// </param>
+        /// <param name="body">
+        /// If your exception was not ignored, this is the body of the MessageBox.
+        /// Defaults to the thrown exception's message.
+        /// </param>
+        public static bool IgnoreSpecificException(Exception exceptionToIgnore, Exception exceptionCaught,
+            bool messageBox = false, string title = "Error", string body = "")
+        {
+            if (exceptionCaught.GetType().FullName == exceptionToIgnore.GetType().FullName)
+                return true;
+
+            if (messageBox)
+                MessageBox.Show(string.IsNullOrEmpty(body) ? exceptionCaught.Message : body, title,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return false;
+        }
+    }
+}
