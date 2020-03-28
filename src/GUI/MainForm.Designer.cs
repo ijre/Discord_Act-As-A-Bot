8ef281546db895027bb0f8cd0b598ded1f77bcc5
Robin Keaton <robinkeatonn@gmail.com>
2020-03-28 19:21:29 -0400

    Added new icon to resources and set it as the MainForm's icon

--- src/GUI/MainForm.Designer.cs
+++ src/GUI/MainForm.Designer.cs
@@ -222,9 +222,9 @@
             this.Controls.Add(this.ServerChannelList);
             this.Controls.Add(this.Multiple_ImagesLB);
             this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(931, 586);
             this.Name = "MainForm";
-            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Act As A Discord Bot";
             this.Output_ChatCM.ResumeLayout(false);
