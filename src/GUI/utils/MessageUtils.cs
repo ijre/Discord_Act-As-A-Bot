5dad72810b41751f169657e23fa9237753031766
Paige <33820042+ijre@users.noreply.github.com>
2020-03-27 21:06:25 -0400

    Renamed and edited PickyCatch, added MessageUtils class

---
+++ src/GUI/utils/MessageUtils.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+
+namespace hatsune_miku.utils
+{
+    public class MessageUtils
+    {
+        public static DiscordMessage GetMessage(DiscordClient client, ulong message, ulong channel)
+        {
+            return client.GetChannelAsync(channel).Result.GetMessageAsync(message).Result;
+        }
+    }
+}
